6aacaa1de13214e99bad4846e2fde2b505597f9b
Rushabh <rs@rushabhs.com>
2016-07-16 08:10:26 -0700

    Process grid size and find start and end positions

--- mazesolver.rb
+++ mazesolver.rb
@@ -1,10 +1,57 @@
 class MazeSolver
+  attr_reader :maze
+  def initialize(filename)
+    lines = File.readlines(filename).map(&:chomp).map
+    @maze = lines.map{ |l| l.split("")}
+    @start_position = coord_of_start_end(@maze, "S")
+    @end_position = coord_of_start_end(@maze, "E")
+    @available_moves = []
+  end
+
+  def [](pos)
+    r, c = pos
+    @maze[r][c]
+  end
+
+  def []=(pos, val)
+    r,c = pos
+    @maze[r][c] = val
+  end
+
+  def row_count
+    @maze.length
+  end
+
+  def col_count
+    @maze[0].length
+  end
+
+  def in_maze?(pos)  #is position within the maze?
+    rows = @maze.length
+    cols = @maze[0].length
+    pos[0].between?(0, rows - 1) && pos[1].between?(0, cols - 1)
+  end
+
+  def is_wall?(pos)
+    self[pos] == "*"
+  end
+
+  def coord_of_start_end(grid, mark)
+    row, col = 0, 0
+    grid.each_with_index do |line, id1|
+      line.each_with_index do |el, id2|
+        if el == mark
+          row = id1
+          col = id2
+        end
+      end
+    end
+    [row, col]
+  end
 
 end
 
-
-
-
+=begin
 
 if __FILE__ == $PROGRAM_NAME
   if ARGV.empty?
@@ -17,3 +64,4 @@ if __FILE__ == $PROGRAM_NAME
     game.play
   end
 end
+=end
