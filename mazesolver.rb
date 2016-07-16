5da9280cfc8e1055540b1eb51beea69b85e60db2
Rushabh <rs@rushabhs.com>
2016-07-16 08:44:26 -0700

    Changed populate_available_positions to populate_available_position_nodes

--- mazesolver.rb
+++ mazesolver.rb
@@ -1,12 +1,15 @@
 require 'byebug'
+require './treenode'
 class MazeSolver
   attr_reader :maze, :seen_positions
   def initialize(filename)
     lines = File.readlines(filename).map(&:chomp).map
     @maze = lines.map{ |l| l.split("")}
     @start_position = coord_of_start_end(@maze, "S")
+    @start_node = PolyTreeNode.new(@start_position)
     @end_position = coord_of_start_end(@maze, "E")
     @seen_positions = []
+    @position_nodes = [@start_node]
   end
 
   def [](pos)
@@ -57,16 +60,25 @@ class MazeSolver
     adjacent_positions.select{|new_pos| !is_wall?(new_pos) && in_maze?(new_pos)}
   end
 
-  def populate_available_positions
+  def populate_available_position_nodes
     #populates all positions from START that are empty using BFS
-    queue = [@start_position]
+    queue = [@start_node]
     until queue.empty?
-      current_position = queue.shift
-      adjacent_positions = find_adjacent(current_position)
+      current_node = queue.shift
+      adjacent_positions = find_adjacent(current_node.value)
       adjacent_positions.reject! { |pos| @seen_positions.include?(pos) }
       @seen_positions.concat(adjacent_positions)
-      queue.concat(adjacent_positions)
+      adjacent_positions.each do |pos|
+        child = PolyTreeNode.new(pos)
+        child.parent = current_node
+        @position_nodes << child
+        queue << child
+      end
     end
+
+  end
+
+  def build_tree
 
   end
 
@@ -75,6 +87,22 @@ class MazeSolver
 end
 
 
+
+p "*" * 15
+m = MazeSolver.new('maze1.txt')
+# p m.in_maze?([-1, -1])
+# p m.in_maze?([100, 100])
+# p m.in_maze?([5, 5])
+# p m.row_count
+# p m.col_count
+# p m.in_maze?([8, 16])
+# p m.in_maze?([7, 16])
+# p m.in_maze?([7, 15])
+p "*" * 15
+p "adjacent_positions"
+# p m.find_adjacent([2,3])
+p m.populate_available_position_nodes
+p m.seen_positions
 
 =begin
 
